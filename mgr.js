d7e2e8b901237621a09649b994fed0ee578142ad
rukianna <ruk.sakmak@gmail.com>
2024-08-12 20:51:19 +0300

    randomn

--- mgr.js
+++ mgr.js
@@ -139,23 +139,23 @@ define(['managerAPI',
             ]
         },
         
-        
-        {inherit: 'intro'},	
-        {
-            mixer: 'wrapper',
-            data:[
-                {inherit: 'explicits'},
+        		
+		{inherit: 'intro'},	
+		{
+			mixer: 'random',
+			data: [
+				{inherit: 'explicits'},
+				// force the instructions to precede the IAT
+				{
+					mixer: 'wrapper',
+					data: [
+						{inherit: 'raceiat_instructions'},
+						{inherit: 'raceiat'}
+					]
+				}
+			]
+		}
 
-                // force the instructions to preceed the iat
-                {
-                    mixer: 'wrapper',
-                    data: [
-                        {inherit: 'raceiat_instructions'},
-                        {inherit: 'raceiat'}
-                    ]
-                }
-            ]
-        },
 
 		{inherit: 'uploading'},
         {inherit: 'lastpage'},
