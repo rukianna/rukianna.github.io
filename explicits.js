edeff8fe88da2257791f083b20d2c25447ffcf3d
rukianna <ruk.sakmak@gmail.com>
2024-09-03 20:14:49 +0300

    rumuz and gender Update explicits.js

--- explicits.js
+++ explicits.js
@@ -6,13 +6,13 @@ define(['questAPI'], function(Quest){
 	* Page prototype
 	*/
     API.addPagesSet('basicPage',{
-        noSubmit:false, // Change to true if you don't want to show the submit button.
+        noSubmit:false, 
         header: 'Demografik Bilgiler',
         decline: true,
         declineText: isTouch ? 'Gec' : 'Soruyu Gec', 
         autoFocus:true, 
-        progressBar:  'Sayfa <%= pagesMeta.number %> / 8',
-	submitText: isTouch ? 'Gonder' : 'Gonder', // Yeni buton metnini burada belirleyin
+        progressBar:  'Sayfa <%= pagesMeta.number %> / 2',
+	submitText: isTouch ? 'Gonder' : 'Gonder', 
     });
 	
     /**
@@ -35,48 +35,15 @@ define(['questAPI'], function(Quest){
         type: 'selectOne'
     });
 
-    API.addQuestionsSet('basicDropdown',{
-        inherit :'basicQ',
-        type : 'dropdown',
-        autoSubmit:false
-    });
-
     /**
 	* Demographic Questions
 	*/
-    // Birth Month
-    API.addQuestionsSet('birthMonth',{
-        inherit: 'basicSelect',
-        name: 'birthMonth',
-        stem: 'Dogum ayiniz nedir?',
-        answers: [
-            {text: 'Ocak', value: 'Ocak'},
-            {text: 'subat', value: 'subat'},
-            {text: 'Mart', value: 'Mart'},
-            {text: 'Nisan', value: 'Nisan'},
-            {text: 'Mayis', value: 'Mayis'},
-            {text: 'Haziran', value: 'Haziran'},
-            {text: 'Temmuz', value: 'Temmuz'},
-            {text: 'Agustos', value: 'Agustos'},
-            {text: 'Eylul', value: 'Eylul'},
-            {text: 'Ekim', value: 'Ekim'},
-            {text: 'Kasim', value: 'Kasim'},
-            {text: 'Aralik', value: 'Aralik'}
-        ]
-    });
-
-    // Birth Year
-    API.addQuestionsSet('birthYear',{
-        inherit: 'basicDropdown',
-        name: 'birthYear',
-        stem: 'Dogum yiliniz nedir?',
-        answers: (function(){
-            let years = [];
-            for (let i = 2005; i >= 1910; i--) {
-                years.push({text: i.toString(), value: i});
-            }
-            return years;
-        })()
+    // Nickname
+    API.addQuestionsSet('nickname',{
+        inherit: 'basicQ',
+        type: 'text',
+        name: 'nickname',
+        stem: 'Lutfen bir rumuz giriniz:'
     });
 
     // Gender
@@ -90,88 +57,12 @@ define(['questAPI'], function(Quest){
         ]
     });
 
-    // Education Level
-    API.addQuestionsSet('educationLevel',{
-        inherit: 'basicSelect',
-        name: 'educationLevel',
-        stem: 'Egitim durumunuz nedir?',
-        answers: [
-            {text: 'Lisans', value: 'Lisans'},
-            {text: 'Lisansustu', value: 'Lisansustu'},
-            {text: 'Doktora', value: 'Doktora'}
-        ]
-    });
-
-    /**
-	* Specific Questions (Relationship Duration, Gender Roles, Relationship with Partner, Belief in Gender Equality)
-	*/
-    // Relationship Duration
-    API.addQuestionsSet('relationshipDuration',{
-        inherit: 'basicSelect',
-        name: 'relationshipDuration',
-        stem: 'Iliski sureniz nedir?',
-        answers: [
-            {text: '0-6 Ay', value: '0-6 Ay'},
-            {text: '6 Ay - 1 Sene', value: '6 Ay - 1 Sene'},
-            {text: '1-2 Yil', value: '1-2 Yil'},
-            {text: '3-5 Yil', value: '3-5 Yil'},
-            {text: '6+ Yil', value: '6+ Yil'}
-        ]
-    });
-
-    // Gender Roles and Attitudes
-    API.addQuestionsSet('genderRolesView',{
-        inherit: 'basicSelect',
-        name: 'genderRolesView',
-        stem: 'Cinsiyet rollerine bakisinizi ve toplumsal cinsiyete yonelik tutumunuzu nasil tanimlarsiniz?',
-        answers: [
-            {text: 'Biraz Esitlikci', value: 'Biraz Esitlikci'},
-            {text: 'Biraz Muhafazakar', value: 'Biraz Muhafazakar'},
-            {text: 'Tamamen Esitlikci', value: 'Tamamen Esitlikci'},
-            {text: 'Tamamen Muhafazakar', value: 'Tamamen Muhafazakar'},
-            {text: 'Bilmiyorum', value: 'Bilmiyorum'}
-        ]
-    });
-
-    // Relationship with Partner
-    API.addQuestionsSet('relationshipEquality',{
-        inherit: 'basicSelect',
-        name: 'relationshipEquality',
-        stem: 'Iliskinizi nasil tanimlarsiniz?',
-        answers: [
-            {text: 'Tamamen Esitlikci', value: 'Tamamen Esitlikci'},
-            {text: 'Esitlikci Degil', value: 'Esitlikci Degil'},
-            {text: 'Biraz Esitlikci', value: 'Biraz Esitlikci'},
-            {text: 'Biraz Esitlikci Degil', value: 'Biraz Esitlikci Degil'},
-            {text: 'Tamamen Esitsiz', value: 'Tamamen Esitsiz'}
-        ]
-    });
-
-    // Belief in Gender Equality
-    API.addQuestionsSet('beliefInEquality',{
-        inherit: 'basicSelect',
-        name: 'beliefInEquality',
-        stem: 'Kadin erkek esitligine ne kadar inaniyorsunuz?',
-        answers: [
-            {text: 'Tamamen Inaniyorum', value: 'Tamamen Inaniyorum'},
-            {text: 'Biraz Inaniyorum', value: 'Biraz Inaniyorum'},
-            {text: 'Tamamen Inanmiyorum', value: 'Tamamen Inanmiyorum'},
-            {text: 'Biraz Inanmiyorum', value: 'Biraz Inanmiyorum'}
-        ]
-    });
-
     /**
 	* Sequence
 	*/
     API.addSequence([
-        {inherit: 'basicPage', questions: {inherit: 'birthMonth'}},
-        {inherit: 'basicPage', questions: {inherit: 'birthYear'}},
-        {inherit: 'basicPage', questions: {inherit: 'gender'}},
-        {inherit: 'basicPage', questions: {inherit: 'educationLevel'}},
-        {inherit: 'basicPage', questions: {inherit: 'relationshipDuration'}},
-        {inherit: 'basicPage', questions: {inherit: 'genderRolesView'}},
-        {inherit: 'basicPage', questions: {inherit: 'relationshipEquality'}},
-        {inherit: 'basicPage', questions: {inherit: 'beliefInEquality'}}
+        {inherit: 'basicPage', questions: {inherit: 'nickname'}},
+        {inherit: 'basicPage', questions: {inherit: 'gender'}}
     ]);
 
     return API.script;
