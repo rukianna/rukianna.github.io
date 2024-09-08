96e8e6c23a0d0fa99b4985d3cba717154f17013d
rukianna <ruk.sakmak@gmail.com>
2024-09-08 23:56:02 +0300

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -190,7 +190,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
 	    stimulusCss : {color:'#336600','font-size':'2.3em'}
 	},
         attribute1 : {
-            name : 'Aile',
+            name : 'Feminen',
             title : {
                 media : {word : 'Feminen'},
                 css : {color:'#0000FF','font-size':'1.8em'},
@@ -208,7 +208,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
             stimulusCss : {color:'#0000FF','font-size':'2.3em'}
         },
         attribute2 : {
-            name : 'Kariyer',
+            name : 'Maskülen',
             title : {
                 media : {word : 'Maskülen'},
                 css : {color:'#0000FF','font-size':'1.8em'},
